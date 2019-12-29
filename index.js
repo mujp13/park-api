9791d0501cfe4464e153dcc24ec0208d918fb2c1
Kevin Park <keunsuk413@gmail.com>
2019-12-28 20:29:34 -0600

    firt commit

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const apiKey = 'Qhe7TbrpnogPIAzaQfBsZuXMxlSjGvHexLHvYjzN';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  
+  for (let i = 0; i < responseJson.data.length; i++) {
+    
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3> 
+      <p><b>State:</b> ${responseJson.data[i].states}</p>
+      <p><b>Description:</b> ${responseJson.data[i].description}</p>
+      <p><b>Link:</b> <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
+      </li>`
+    );
+  }
+
+  $('#results').removeClass('hidden');
+}
+
+function getYouTubeVideos(query, maxResults = 10) {
+  const params = {
+    api_key: apiKey,
+    q: query,
+    limit: maxResults
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getYouTubeVideos(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
